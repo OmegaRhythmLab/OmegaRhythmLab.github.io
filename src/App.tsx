2c2dec8ac4cec58e7b5ce58b4a6a19bcb48ef9f4
giraf <giraffat@outlook.com>
2022-12-31 09:23:49 +0800

    完成主页

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 import '@fontsource/roboto/300.css';
@@ -7,24 +6,79 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import {
+    AppBar,
+    CssBaseline,
+    Tab,
+    Tabs,
+    Toolbar,
+    Typography,
+    Stack,
+    SxProps,
+    useTheme,
+    useMediaQuery, Box
+} from '@mui/material'
+import HomePage from './HomePage'
+
+enum TabItem {
+    Home,
+    Download,
+    About
+}
+
+interface ContentProps {
+    tabItem: TabItem,
+    sx?: SxProps
+}
+
+function Content(props: ContentProps) {
+    const screenWidthUpMdQuery = useTheme().breakpoints.up("md")
+    const isScreenWidthUpMd = useMediaQuery(screenWidthUpMdQuery)
+    let contentComponent: JSX.Element
+
+    switch (props.tabItem) {
+        case TabItem.Download:
+            contentComponent = <></>
+            break
+        case TabItem.About:
+            contentComponent = <></>
+            break
+        case TabItem.Home:
+            contentComponent = (
+                <HomePage screenWidthUpMdQuery={screenWidthUpMdQuery} screenWidthUpMd={isScreenWidthUpMd}/>
+            )
+            break
+    }
+
+    return (<Box sx={props.sx}>{contentComponent}</Box>)
+}
+
 function App() {
+    const [tabItem, setTabItem] = useState(TabItem.Home)
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Stack spacing={2} bgcolor="background">
+            <CssBaseline/>
+            <AppBar>
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{"flexGrow": 1}}>Omega</Typography>
+                    <Tabs value={tabItem} onChange={(event, value) => setTabItem(value)}
+                          textColor="inherit" indicatorColor="secondary"
+                          sx={{
+                              alignSelf: "stretch",
+                              ".MuiTabs-scroller": {"display": "flex"},
+                              ".MuiTabs-indicator": {"height": 4}
+                          }}>
+                        <Tab label="主页" value={TabItem.Home}/>
+                        <Tab label="下载" value={TabItem.Download}/>
+                        <Tab label="关于" value={TabItem.About}/>
+                    </Tabs>
+                </Toolbar>
+            </AppBar>
+
+            <Toolbar/>
+            <Content tabItem={tabItem} sx={{alignSelf: "center"}}/>
+        </Stack>
     );
 }
 
---
+++ src/HomePage.tsx
@@ -0,0 +1,86 @@
+import {Button, Card, CardContent, CardMedia, Container, Stack, SxProps, Typography} from '@mui/material'
+import imageUrl from './image/cover.jpg'
+import React from 'react'
+
+interface News {
+    title: string
+    coverUrl: string
+    author: string
+}
+
+export class MediumSizedNews implements News {
+    author: string
+    coverUrl: string
+    title: string
+    description: string
+
+    constructor(author: string, coverUrl: string, title: string, description: string) {
+        this.author = author
+        this.coverUrl = coverUrl
+        this.title = title
+        this.description = description
+    }
+}
+
+export class LargeSizedNews implements News {
+    author: string
+    coverUrl: string
+    title: string
+
+    constructor(author: string, coverUrl: string, title: string) {
+        this.author = author
+        this.coverUrl = coverUrl
+        this.title = title
+    }
+}
+
+let news = new Array<News>()
+for (let i = 0; i < 20; i++) {
+    news.push(new MediumSizedNews("此处应有标题", imageUrl, "此处应有作者", "此处应有简介"));
+    news.push(new LargeSizedNews("此处应有标题", imageUrl, "此处应有作者"));
+    news.push(new MediumSizedNews("此处应有标题", imageUrl, "此处应有作者", "此处应有简介"));
+}
+
+export default function HomePage(props: { sx?: SxProps, screenWidthUpMdQuery: string, screenWidthUpMd: boolean }) {
+    return <Container maxWidth="lg" sx={props.sx}>
+        <Stack spacing={2}>
+            <Card sx={{position: 'relative'}}>
+                <CardMedia component="img" image={imageUrl} title="封面图片"/>
+                <CardContent
+                    sx={{[props.screenWidthUpMdQuery]: {position: 'absolute', left: 0, top: 0, padding: 8}}}>
+                    <Typography variant={props.screenWidthUpMd ? 'h3' : 'h4'} component="div" gutterBottom>
+                        恶化脑裂病情的最佳方式
+                    </Typography>
+                    <Button variant="contained" sx={{alignSelf: 'start', paddingLeft: 8, paddingRight: 8}}>
+                        下载
+                    </Button>
+                </CardContent>
+            </Card>
+
+            <Stack sx={{flexWrap: 'wrap', flexDirection: 'row'}}>{news.map((value) => {
+                function flexGrow() {
+                    if (value instanceof MediumSizedNews) {
+                        return 2
+                    } else if (value instanceof LargeSizedNews) {
+                        return 3
+                    } else {
+                        throw new TypeError()
+                    }
+                }
+
+                return (
+                    <Card sx={{flex: flexGrow(), minWidth: 300, margin: 2}}>
+                        <CardMedia component="img" image={value.coverUrl} title={value.title}/>
+                        <CardContent>
+                            <Typography gutterBottom variant="h5"
+                                        component="div">{value.title}</Typography>
+                            <Typography variant="body2"
+                                        color="text.secondary">{value.author}</Typography>
+                        </CardContent>
+                    </Card>
+                )
+            })}
+            </Stack>
+        </Stack>
+    </Container>
+}
